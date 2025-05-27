e85aaa7dd850aa769dc3a51ff3cbd88fd0cdc81a
IshahBautista <kofishah3@gmail.com>
2025-05-27 16:27:28 +0800

    added new character sheet

---
+++ enemy.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+@onready var anim = $AnimatedSprite2D
+@export var speed = 200
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed	
+
+	#if there is horizontal movement
+	if velocity.x > 0:
+		anim.play("walk_right")
+	elif velocity.x < 0:
+		anim.play("walk_left")
+	#if there is vertical movement, but no horizontal movement
+	elif velocity.y > 0:
+		anim.play("walk_down")
+	elif velocity.y < 0:
+		anim.play("walk_up")
+	
+	#if no movement, stop animation
+	if velocity.x == 0 and velocity.y == 0:
+		anim.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
